4327cbf582f20d9c8772534eb456227a0a0ef749
DanteAdami <DanteAdami>
2023-11-11 12:43:37 +0100

    necessary files for making the repo pip installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='dsss_homework_2',
+    version='1.0',
+    author='DanteAdami',
+    description='DSSS homework2 - Development of a math game integrating unit tests',
+    url='https://github.com/DanteAdami/dsss_homework_2',
+    packages=['math_quiz'],
+    license='MIT'
+)
