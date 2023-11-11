f93615a537cefd863c658d583e52ce36fcc95ede
DanteAdami <DanteAdami>
2023-11-11 12:46:47 +0100

    update of setup file

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='dsss_homework_2',
-    version='1.0',
+    name='dsss_homework_2.1',
+    version='2.0',
     author='DanteAdami',
     description='DSSS homework2 - Development of a math game integrating unit tests',
     url='https://github.com/DanteAdami/dsss_homework_2.1.git',
