6ea0a3e03b65c9eff057013a8de8fa5f10f08b9e
DanteAdami <DanteAdami>
2023-11-11 12:37:07 +0100

    cleanup of the code

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,81 @@
 import random
 
 
-def function_A(min, max):
+def function_random_integer(min, max):
     """
     Random integer.
     """
     return random.randint(min, max)
 
 
-def function_B():
+def function_random_sign():
+    """
+    Select a random sign operation between sum, difference and multiplication
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def function_operation(value1, value2, sign):
+    """
+    Return the operation that is being performed as an expression together, and the result of the 
+    expression. 
+    """
+    expression = f"{value1} {sign} {value2}" #Create the expression putting together the three eleemnts
+    
+    if sign == '+': #check the sign and perform the according operation
+        result = value1 + value2
+    elif sign == '-': 
+        result = value1 - value2
+    else: 
+        result = value1 * value2
+
+    return expression, result
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """
+    Perform a little game. The system will generate some random operations, and the user has to guess the
+    result. The game is repeated the number of times of the variable repeats. If the user fails the question, 
+    the correct answer is shown. At the end of the game the final result of the user is shown. 
+    """
+
+    points = 0 # initialize the points of the user at 0
+    repeats = 3 # set the number of iterations of the game
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(repeats):
+        
+        # Creating the operations by randomly selecting two integers and one sign
+        n1 = function_random_integer(1, 10); 
+        n2 = function_random_integer(1, 5); 
+        o = function_random_sign()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        # get the expression and the result and show the expression to the user
+        PROBLEM, ANSWER = function_operation(n1, n2, o)
         print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
 
+        # get the input from the user with their guess. We make sure that the input is an integer
+        while True:
+            try:
+                useranswer = input("Your answer: ")
+                useranswer = int(useranswer)
+                break # if integer, we break the while loop
+            except ValueError:
+                # if not integer, we remember to the user the requirement and wait for a new input
+                print("Invalid input! The input has to be an integer. Please enter a valid integer.")
+
+
+        # if the user guesses the correct answer, then they get a point and a message is shown.
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            points += 1
         else:
+            # if the user is wrong, the correct answer is shown
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    # at the end of the game, we show the final result. 
+    print(f"\nGame over! Your score is: {points}/{repeats}")
 
 if __name__ == "__main__":
     math_quiz()
--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,42 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import function_random_integer, function_random_sign, function_operation
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_function_random_integer(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = function_random_integer(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_function_random_sign(self):
+        # Test to check if the operator generated is within the options
+        sign_plus = 0
+        for _ in range(1000): # Test a large number of random inputs
+            sign = function_random_sign()
+            self.assertTrue(sign == '+' or sign == '-' or sign == '*')
 
-    def test_function_C(self):
+
+    def test_function_operation(self):
+            # Test the function operation. We first create some sample operations
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (8, 3, '-', '8 - 3', 5),
+                (4, 6, '*', '4 * 6', 24),
+                (9, 9, '-', '9 - 9', 0),
+                (5, 0, '*', '5 * 0', 0),
+                (1, 5, '+', '1 + 5', 6)
             ]
-
+            # now for each operation, we make sure that the output of the function is the expected one
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+                expression, result = function_operation(num1, num2, operator)
+                self.assertTrue(expression == expected_problem and result == expected_answer)
 
 if __name__ == "__main__":
     unittest.main()
+
+
+    
