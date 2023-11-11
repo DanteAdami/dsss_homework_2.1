5cda06e84add23de88db28a8e41aeb2bfbe39cba
DanteAdami <DanteAdami>
2023-11-11 12:45:52 +0100

    update of setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0',
     author='DanteAdami',
     description='DSSS homework2 - Development of a math game integrating unit tests',
-    url='https://github.com/DanteAdami/dsss_homework_2',
+    url='https://github.com/DanteAdami/dsss_homework_2.1.git',
     packages=['math_quiz'],
     license='MIT'
 )
